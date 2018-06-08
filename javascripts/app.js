dc8fadb5ca39a41eb2b321e60d358f52da4ad425
Alina Kitaeva <akitaeva@gmail.com>
2018-06-08 01:24:11 -0400

    getCommand and executeCommand functions are added

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,6 +6,8 @@ var rover = {
   y: 0
 }
 
+var cmd;
+
 // ======================
 function turnLeft(rover){
   console.log("turnLeft was called!");
@@ -74,3 +76,51 @@ function moveForward(rover){
   console.log(`(${rover.x}, ${rover.y})`);
 
 }
+
+function getCommand(cmd) {
+  var userCmd = prompt("Please enter the desired route. Type 'F' for forward', 'L' for the left turn, 'R' for the right turn");
+  if (userCmd != null) {
+    var userCmdLc = userCmd.toLowerCase();
+    var seqLength = userCmd.length;
+    var n;
+    for (n=0; n < seqLength; n++) {
+      if (userCmdLc[n]==f || userCmdLc[n]==l || userCmdLc[n]==r) {
+        cmd += userCmdLc[n];
+      }
+      else {
+        console.log("The "+userCmdLc[n]+" is not recognized");
+      }
+    console.log("The approved route is " + cmd);
+    return(cmd);
+    }
+  }
+  else { 
+    console.log("No route given");
+  }
+} 
+  
+  
+function executeCommand(cmd) {
+  var len = cmd.length;
+  var step;
+  for (step = 0; step < len; step++){
+    if (cmd[step]=='f') {
+      moveForward();
+    }
+    else if (cmd[step]=='l') {
+      turnLeft();
+    } 
+    else if (cmd[step]=='r') {
+      turnRight();
+    }
+    
+
+  
+
+  } 
+
+}
+    
+
+
+
